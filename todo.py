1ce5f63a8b8b251a3d5a072398579b2496e9dc49
Shivam Kumar <shivamku9107@gmail.com>
2025-11-22 14:50:07 +0530

    Adding Comments

--- todo.py
+++ todo.py
@@ -1,5 +1,8 @@
+
+#initailize empty array for store todos
 todoList = []
 
+#open file and read it, if it is avilable if not, pass or skip it
 try:
     with open("todo.txt", "r") as f:
         for line in f:
@@ -7,7 +10,7 @@ try:
 except FileNotFoundError:
     pass
 
-
+#a function for READ the ToDos
 def readToDo():
   if(len(todoList) == 0):
     print("The list is empty!")
@@ -16,42 +19,60 @@ def readToDo():
     for i in range(len(todoList)):
       print(str(i+1 )+ ") " + todoList[i])
 
-def addToDO():
+#a function for ADD a ToDo
+def addToDo():
   todoList.append(input("Enter the To-do: "))
-  print("The To-Do ADDED successfully!\n")
+  print("\nThe To-Do ADDED successfully!\n")
   readToDo()
 
-def updateToDO():
+#a function for UPDATE a ToDo
+def updateToDo():
   indexOfTodo = (int(input("Enter thier Serial number to UPDATE: "))- 1)
   todoList[indexOfTodo] = input("Enter updated ToDo: ")
-  print("The To-Do UPDATED successfully!\n")
+  print("\nThe To-Do UPDATED successfully!\n")
   readToDo()
 
+#a function for DELETE a ToDo
 def deleteToDo():
   todoList.pop(int(input("Enter thier Serial number to DELETE: "))- 1)
   
-  print("The data was DELETED succssfully!\n")
+  print("\nThe data was DELETED succssfully!\n")
   readToDo()
 
+#a function for CLEAR a ToDo
+def clearToDo():
+  todoList.clear()
+
+  print("\nThe data was CLEAR succssfully!\n")
+  readToDo()
+
+
+
+#it is a function for select the oprations such as READ, WRITE, UPADTE, DELETE, and EXIT
 def selection():
 
-  print("\n1 stands for ADD To-Do\n2 stands for UPDATE To-Do\n3 stands for READ To-Dos\n4 stands for DELETE To-Do\n5 for EXIT from Terminal\n")
+  print("\n1 stands for ADD To-Do\n2 stands for UPDATE To-Do\n3 stands for READ To-Dos\n4 stands for DELETE To-Do\n5 for CLEAR all To-Dos\n6 for EXIT from Terminal\n")
+
+  #"want" is a variable that stores user input for performing operation
   want = int(input("What do you want: "))
 
   if(want == 1):
-    addToDO()
+    addToDo()
   elif(want == 2):
-    updateToDO()
+    updateToDo()
   elif(want == 3):
     readToDo()
   elif(want == 4):
     deleteToDo()
   elif(want == 5):
-    print("Thank you!")
+    clearToDo()
+  elif(want == 6):
+    print("\nThank you!\n")
     exit()
   else:
-    print("Invalid option selected.")
+    print("\nInvalid option selected.\n")
 
+#it is a loop for selecting n time of oprations, it will never stop untill user is setisfied...
 while(True):
   selection()
   with open("todo.txt", "w") as f:
